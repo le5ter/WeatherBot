1dce8821791c8f35f4cb057a2ddb5713349e8610
Alexey <al.soshnikov@icloud.com>
2023-08-24 22:54:37 +0300

    Bot v1.1.1 : Timezone fix, Buttons fix

--- handlers/getting_info.py
+++ handlers/getting_info.py
@@ -1,6 +1,6 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardRemove, ReplyKeyboardMarkup
 from handlers.common import GetInfo
 import aiohttp
 import os
@@ -10,14 +10,12 @@ import datetime
 load_dotenv(find_dotenv())
 router = Router()
 
-
 url = "https://api.gismeteo.net/v2/search/cities/?query="
 url2 = "https://api.gismeteo.net/v2/weather/current/"
 headers = {
     "X-Gismeteo-Token": f'{os.getenv("API_TOKEN")}',
     "Accept-Encoding": "gzip"
 }
-
 
 wind_dict = {0: "Спокойный",
              1: "Северный",
@@ -40,7 +38,7 @@ async def choosing_city(message: Message, state: FSMContext):
     else:
         user_data = await state.get_data()
         async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(url+input_city) as response:
+            async with session.get(url + input_city) as response:
                 json_body = await response.json()
                 code = int(json_body['meta']['code'])
                 if code == 200:
@@ -48,8 +46,22 @@ async def choosing_city(message: Message, state: FSMContext):
                         if json_body["response"]["total"] > 0:
                             city_id: int = json_body["response"]["items"][0]["id"]
                             await state.update_data(city_id=city_id)
-                            await message.answer("Выберите промежуток")
                             await state.set_state(GetInfo.getting_period)
+
+                            kb = [
+                                [
+                                    KeyboardButton(text="Сейчас"),
+                                    KeyboardButton(text="Завтра"),
+                                    KeyboardButton(text="3 Дня")
+                                ],
+                            ]
+                            keyboard = ReplyKeyboardMarkup(
+                                keyboard=kb,
+                                resize_keyboard=True,
+                                input_field_placeholder="Выберите промежуток"
+                            )
+
+                            await message.answer("Выберите промежуток", reply_markup=keyboard)
                         elif json_body["response"]["total"] == 0:
                             await message.answer("Город не найден, попробуйте еще раз")
                     except KeyError:
@@ -61,45 +73,35 @@ async def choosing_city(message: Message, state: FSMContext):
                     await message.answer("Ошибка сервера, попробуйте позже.")
 
 
-@router.message(GetInfo.getting_period)
+@router.message(GetInfo.getting_period, F.text.lower() == "сейчас")
 async def choosing_period(message: Message, state: FSMContext):
     user_data = await state.get_data()
     city_id: int = user_data['city_id']
-    kb = [
-        [
-            KeyboardButton(text="Сейчас"),
-            KeyboardButton(text="Завтра"),
-            KeyboardButton(text="3 Дня")
-        ],
-    ]
-    keyboard = ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите промежуток"
-    )
-    # 'https://api.gismeteo.net/v2/weather/forecast/4368/?lang=en&days=1'
-    if message.text.lower() == "сейчас":
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(f"{url2}{city_id}") as response:
-                json_body = await response.json()
-        city: str = user_data['input_city']
-        now = datetime.datetime.now()
-        format_time = f'{now:%d-%m-%Y %H:%M}'
-        description = json_body['response']['description']['full']
-        air_temperature = json_body['response']['temperature']['air']['C']
-        water_temperature = json_body['response']['temperature']['water']['C']
-        humidity = json_body['response']['humidity']['percent']
-        pressure = json_body['response']['pressure']['mm_hg_atm']
-        wind_direction = json_body['response']['wind']['direction']['scale_8']
-        wind_speed = json_body['response']['wind']['speed']['m_s']
 
-        weather_result = f'{city:^30}\n' \
-                  f'{format_time:^30}\n' \
-                  f'{description}\n' \
-                  f'Температура воздуха: {air_temperature}°C\n' \
-                  f'Температура воды: {water_temperature}°C\n' \
-                  f'Влажность: {humidity}%\n' \
-                  f'Давление: {pressure} мм рт. ст.\n' \
-                  f'Ветер: {wind_dict[int(wind_direction)]} {wind_speed} м/с\n' + '*' * 30 +\
-                  f'\n\n Информация о погоде взята с сайта [Gismeteo](gismeteo.ru)'
-        await message.answer(weather_result, parse_mode="MarkdownV2")
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(f"{url2}{city_id}") as response:
+            json_body = await response.json()
+    city: str = user_data['input_city']
+    offset = json_body['response']['date']['time_zone_offset']
+    offset /= 60
+    tz = datetime.timezone(datetime.timedelta(hours=offset), name='МСК')
+    now = datetime.datetime.now(tz=tz)
+    format_time = f'{now:%d-%m-%Y %H:%M}'
+    description = json_body['response']['description']['full']
+    air_temperature = json_body['response']['temperature']['air']['C']
+    water_temperature = json_body['response']['temperature']['water']['C']
+    humidity = json_body['response']['humidity']['percent']
+    pressure = json_body['response']['pressure']['mm_hg_atm']
+    wind_direction = json_body['response']['wind']['direction']['scale_8']
+    wind_speed = json_body['response']['wind']['speed']['m_s']
+
+    weather_result = f'{city:*^30}\n' \
+                     f'{format_time:*^33}\n' \
+                     f'{description}\n' \
+                     f'Температура воздуха: {air_temperature}°C\n' \
+                     f'Температура воды: {water_temperature}°C\n' \
+                     f'Влажность: {humidity}%\n' \
+                     f'Давление: {pressure} мм рт. ст.\n' \
+                     f'Ветер: {wind_dict[int(wind_direction)]} {wind_speed} м/с\n' + '*' * 31 + \
+                     f'\n\n Информация о погоде взята с сайта <a href="gismeteo.ru">Gismeteo</a>'
+    await message.answer(weather_result, parse_mode="HTML", reply_markup=ReplyKeyboardRemove())
